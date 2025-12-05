34d7895b29b02b4aac04bffc1f499852c0404892
lamabalde <lamaranabalde812@gmail.com>
2025-12-05 02:31:49 +0000

    update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -89,13 +89,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # }
 
 # Prod: Render PostgreSQL
+DATABASES = {}
 DATABASE_URL = os.environ.get("DATABASE_URL")
+
 if DATABASE_URL:
+    # Prod: PostgreSQL sur Render
     DATABASES["default"] = dj_database_url.config(
         default=DATABASE_URL,
         conn_max_age=600,
         ssl_require=True
     )
+else:
+    # Dev: SQLite
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 
 # ======================
 # PASSWORD VALIDATION
